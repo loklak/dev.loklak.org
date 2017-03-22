6aef3e64c225fbb39e15be7c7d21ca0611731ac0
Kaisar Arkhan (Yuki) <ykno@protonmail.com>
2017-03-22 19:06:42 +0700

    Add subtree pull script with config file

---
+++ .ci/pull.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Take file location from first argument
+CONFIG=$1
+
+# Keep old input file separator
+OLDIFS=$IFS
+# Change it to ,
+IFS=,
+
+# Exit if config file doesn't exist
+if [ ! -f $CONFIG ]; then
+    echo "$CONFIG not found"
+    exit 1
+fi
+
+# Parse file
+while read prefix repo branch; do
+    git subtree pull --prefix=$prefix $repo $branch --squash \
+        -m "Update $prefix subtree"
+done < $CONFIG
+
+# Change IFS to the old one
+IFS=$OLDIFS
